bf4ff62ff1f3bfa3de122a7bea9071a4e6ba8b41
mahajanjayesh <officialjayesh@gmail.com>
2019-06-13 22:39:01 +0530

    initial commit

---
+++ js/index_svg.js
@@ -0,0 +1,11 @@
+/* Shapes */
+var svgContainer = document.getElementById('svgContainer');
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer,
+  animType: 'svg',
+  loop: true,
+ 
+ 	path: 'https://s3.ap-south-1.amazonaws.com/jmportfolio-image-header/3046.json'
+ 	
+}
+  );
---
+++ js/index_svg1.js
@@ -0,0 +1,11 @@
+/* Shapes */
+var svgContainer = document.getElementById('svgContainerOne');
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer,
+  animType: 'svg',
+  loop: true,
+  
+ 	
+ 	path:'https://s3.ap-south-1.amazonaws.com/jmportfolio-image-header/3151.json'
+}
+  );
