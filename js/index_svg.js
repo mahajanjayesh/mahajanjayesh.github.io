727b73daf5d2d06a70a23a5bf501202c61b21f33
mahajanjayesh <officialjayesh@gmail.com>
2019-06-13 22:52:51 +0530

    initial commit

--- js/index_svg.js
+++ js/index_svg.js
@@ -5,7 +5,7 @@ var animItem = bodymovin.loadAnimation({
   animType: 'svg',
   loop: true,
  
- 	path: 'https://s3.ap-south-1.amazonaws.com/jmportfolio-image-header/3046.json'
+ 	path: 'https://assets5.lottiefiles.com/datafiles/6WfDdm3ooQTEs1L/data.json'
  	
 }
   );
